6a7028f56c74b43b9236615aaa14a01eec47055f
luki <nibbble@mail.ru>
2025-03-23 23:21:51 +0300

    Lab2 done.

---
+++ lab1/lab1.sql
@@ -0,0 +1,53 @@
+PRAGMA foreign_keys = ON;
+
+-- Таблица оружия
+CREATE TABLE Weapon (
+    name TEXT PRIMARY KEY,
+    basic_damage REAL NOT NULL,
+    damage_type TEXT NOT NULL,
+    rarity INTEGER,
+    price REAL,
+    description TEXT
+);
+
+-- Таблица игроков
+CREATE TABLE Player (
+    id INTEGER PRIMARY KEY,
+    fraction TEXT,
+    name TEXT
+);
+
+-- Таблица сущностей (владение оружием)
+CREATE TABLE Entity (
+    id INTEGER PRIMARY KEY,
+    owner_id INTEGER NOT NULL,
+    weapon_name TEXT NOT NULL,
+    weapon_level INTEGER NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES Player(id) ON DELETE CASCADE,
+    FOREIGN KEY (weapon_name) REFERENCES Weapon(name) ON DELETE CASCADE
+);
+
+-- Таблица слабостей существ
+CREATE TABLE Weaknesses (
+    creature TEXT NOT NULL,
+    damage_type TEXT NOT NULL,
+    damage_modifier INTEGER NOT NULL,
+    PRIMARY KEY (creature, damage_type)
+    FOREIGN KEY (creature) REFERENCES Bestiary(creature) ON DELETE CASCADE
+);
+
+-- Таблица бестиария
+CREATE TABLE Bestiary (
+    creature TEXT PRIMARY KEY,
+    biome TEXT NOT NULL,
+    danger INTEGER NOT NULL
+);
+
+-- Таблица фракций
+CREATE TABLE Fraction (
+    fraction_name TEXT PRIMARY KEY,
+    weapon_name TEXT,
+    enemy_name TEXT,
+    FOREIGN KEY (weapon_name)   REFERENCES Weapon  (name)       ON DELETE CASCADE
+    FOREIGN KEY (enemy_name)    REFERENCES Bestiary(creature)   ON DELETE CASCADE
+);
---
+++ lab2/lab2.sql
@@ -0,0 +1,58 @@
+-- total cost of all weapons with rarity >= 8
+SELECT  CHAR(10) || CHAR(10) || 'total cost of all weapons with rarity >= 8' AS message;
+select sum(price) from Weapon where rarity >= 8 group by (select (0));
+
+-- sort by damage type and than by name
+SELECT  CHAR(10) || CHAR(10) || 'sort by damage type and than by name' AS message;
+select name, damage_type from Weapon order by damage_type, name;
+
+-- number of weapons of every damage type
+SELECT  CHAR(10) || CHAR(10) || 'number of weapons of every damage type' AS message;
+select damage_type, count(name) from Weapon group by damage_type;
+
+-- max rarity of listed weapons
+SELECT  CHAR(10) || CHAR(10) || 'max rarity of listed weapons' AS message;
+select max(rarity) from Weapon group by (select 0);
+
+-- top list sorted by rarity
+SELECT  CHAR(10) || CHAR(10) || 'top list sorted by rarity' AS message;
+select rarity, name, damage_type
+    from Weapon
+    where rarity >= (select max(rarity) from Weapon group by (select 0))
+    order by rarity desc
+;
+-- delete rarity, name, damage_type from Weapon where rarity >= (select max(rarity) from Weapon group by (select 0)) order by rarity desc;
+
+
+-- mean price for weapon of every damage type
+SELECT  CHAR(10) || CHAR(10) || 'mean price for weapon of every damage type' AS message;
+select damage_type, avg(price) from Weapon group by damage_type;
+
+
+-- price median
+SELECT  CHAR(10) || CHAR(10) || 'price median' AS message;
+select avg(price) from (
+    select price
+        from Weapon
+        order by price
+        limit (2 - (select count(name) from Weapon group by (select 0)) %2 )
+        OFFSET ((select count(name) from Weapon group by (select 0)) -1) / 2
+);
+
+
+SELECT  CHAR(10) || CHAR(10) || 'figure out the most popular damage type' AS message;
+-- select damage_type, count(name)
+--     from Weapon
+--     group by damage_type
+-- ;
+
+-- figure out the most popular damage type
+select damage_type
+    from (
+        select damage_type, count(name) as count
+        from Weapon
+        group by damage_type
+    )
+    order by count desc
+    limit 1
+;
